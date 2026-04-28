4a96926531ae1846650cf1e38e67d7ca112dd9d2
Vicky-R720 <andrianavalonavictorien@gmail.com>
2026-04-28 09:47:58 +0200

    commit 2

---
+++ backOffice/src/main/java/com/auth/entity/Role.java
@@ -0,0 +1,24 @@
+package com.auth.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_Roles;
+    private String nom;
+
+    @ManyToMany
+    @JoinTable(
+        name = "role_permission",
+        joinColumns = @JoinColumn(name = "Id_Roles"),
+        inverseJoinColumns = @JoinColumn(name = "Id_Permission")
+    )
+    private Set <Permission> permissions;
+}
